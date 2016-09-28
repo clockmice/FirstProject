01f6e3b58d22e38a62eaccb01a1d4b2a4dcd4d62
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-09-28 09:27:35 +0200

    Initial commit

---
+++ src/main/java/com/academy/FirstProjectApplication.java
@@ -0,0 +1,12 @@
+package com.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+}
