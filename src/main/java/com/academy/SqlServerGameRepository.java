3866d05c19d25210075617053a23808248e7ad65
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-09-28 12:00:24 +0200

    user class added, repository changed

--- src/main/java/com/academy/SqlServerGameRepository.java
+++ src/main/java/com/academy/SqlServerGameRepository.java
@@ -4,6 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class SqlServerGameRepository implements GameRepository {
@@ -11,6 +17,20 @@ public class SqlServerGameRepository implements GameRepository {
     @Autowired
     private DataSource dataSource;
 
+    @Override
+    public List<User> ListUsers() {
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT Name FROM [Project1_5]")) {
+                 List<User> users = new ArrayList<>();
+                 while (rs.next()) users.add(rsUser(rs));
+                 return users;
+        } catch (SQLException e) {
+            throw new GameRepositoryException(e);
+        }
+    }
 
-
+    private User rsUser(ResultSet rs) throws SQLException {
+        return new User(rs.getString("Name"));
+    }
 }
