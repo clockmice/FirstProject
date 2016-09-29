9402e6e7e63ca4e3e63a8ff612fd62850a71b78a
Kim <kim_caprett@hotmail.com>
2016-09-29 11:14:11 +0200

    Kim2 HTML

--- src/main/java/com/academy/GameController.java
+++ src/main/java/com/academy/GameController.java
@@ -14,18 +14,27 @@ public class GameController {
     @Autowired
     private GameRepository gameRepository;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/problem1")
+
+    @RequestMapping(method = RequestMethod.GET, path = "/lastpage")
+    public ModelAndView lastpage() {
+        ModelAndView modelAndView = new ModelAndView("lastpage");
+        return modelAndView;
+    } // Länkar till lastpage från index.html
+
+
+    /*@RequestMapping(method = RequestMethod.GET, path = "/problem1")
     public ModelAndView problem1() {
         ModelAndView modelAndView = new ModelAndView("problem1");
         return modelAndView;
-    } // Länkar till problem1.html
+    } // Länkar till problem1.html */
+
 
     @RequestMapping(method = RequestMethod.POST, path="/post")
     public ModelAndView nameVar(@RequestParam String nameVar, HttpSession session){
         session.setAttribute("nameVar", nameVar);
         //metod som sparar i databasen
         gameRepository.saveName(nameVar);
-        ModelAndView modelAndView = new ModelAndView("redirect:/");
+        ModelAndView modelAndView = new ModelAndView("problem1");
         return modelAndView;
     } // Lägger name i en variabel.
 
@@ -42,28 +51,26 @@ public class GameController {
     } // Länkar till problem2 om svaret är rätt. Annars tillbaka till problem1.
 
 
-//
-//	@RequestMapping(method = RequestMethod.POST, path = "/problem3")
-//	public ModelAndView problem3(@RequestParam String solution1) {
-//		if (solution1.trim().equals("buzz")) {
-//			ModelAndView modelAndView = new ModelAndView("problem3");
-//			return modelAndView;
-//		}
-//		ModelAndView modelAndViewError2 = new ModelAndView("problem2");
-//		modelAndViewError2.addObject("Message2", "Wrong answer!");
-//		return modelAndViewError2;
-//	} // Länkar till problem 3 om svaret är rätt. Annars tillbaka till problem2.
-//
-//	@RequestMapping(method = RequestMethod.POST, path = "/problem4")
-//	public ModelAndView problem4(@RequestParam String solution2) {
-//		if (solution2.trim().equals("1995")) {
-//			ModelAndView modelAndView = new ModelAndView("problem4");
-//			return modelAndView;
-//		}
-//		ModelAndView modelAndViewError3 = new ModelAndView("problem3");
-//		modelAndViewError3.addObject("Message3", "Wrong answer!");
-//		return modelAndViewError3;
-//	} // Länkar till problem 3 om svaret är rätt. Annars tillbaka till problem2.
+	@RequestMapping(method = RequestMethod.POST, path = "/problem3")
+	public ModelAndView problem3(@RequestParam String solution1) {
+		if (solution1.trim().equals("buzz")) {
+			ModelAndView modelAndView = new ModelAndView("problem3");
+			return modelAndView;
+		}
+		ModelAndView modelAndViewError2 = new ModelAndView("problem2");
+		modelAndViewError2.addObject("Message2", "Wrong answer!");
+		return modelAndViewError2;
+	} // Länkar till problem 3 om svaret är rätt. Annars tillbaka till problem2.
 
 
+	@RequestMapping(method = RequestMethod.POST, path = "/problem4")
+	public ModelAndView problem4(@RequestParam String solution2) {
+		if (solution2.trim().equals("1995")) {
+			ModelAndView modelAndView = new ModelAndView("problem4");
+			return modelAndView;
+		}
+		ModelAndView modelAndViewError3 = new ModelAndView("problem3");
+		modelAndViewError3.addObject("Message3", "Wrong answer!");
+		return modelAndViewError3;
+	} // Länkar till problem 4 om svaret är rätt. Annars tillbaka till problem3.
 }
